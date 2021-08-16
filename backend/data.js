c7195bc48025ee0b26a71f57036c03fe0786d597
Ahmad Salihu <Ahmadsalihu419@gmail.com>
2021-08-16 23:35:20 +0100

    code upload

---
+++ backend/data.js
@@ -0,0 +1,151 @@
+
+const data = {
+  products : [
+		{
+			_id: 1,
+      name: 'Nice T-shirt',
+      category: 'Shirts',
+      image: '/productImages/p1.jpg',
+      price: 100,
+      countInStock: 10,
+      brand: 'Nice',
+      rating: '4.5',
+      numReviews: 11,
+      description: 'High Quality Slim Shirts'
+		},
+		{
+			_id: 2,
+      name: 'Nike Slim Pants',
+      category: 'jogger',
+      image: '/productImages/p2.jpg',
+      price: 2500,
+      countInStock: 5,
+      brand: 'Nike',
+      rating: '3.5',
+      numReviews: 15,
+      description: 'High Quality Slim Pants'
+		},
+		{
+			_id: 3,
+      name: 'Gucci Shoes',
+      category: 'Shoes',
+      image: '/productImages/p3.jpg',
+      price: 100,
+      countInStock: 0,
+      brand: 'Gucci',
+      rating: '3.5',
+      numReviews: 20,
+      description: 'Top Best Quality  Shoes'
+		},
+		{
+			_id: 4,
+      name: 'Versace Shirts',
+      category: 'Versace',
+      image: '/productImages/p4.jpg',
+      price: 100,
+      countInStock: 12,
+      brand: 'Varsace',
+      rating: '3.5',
+      numReviews: 8,
+      description: 'Comfortable Swift Categan Shirt'
+		},
+		{
+			_id: 5,
+      name: 'Puma',
+      category: 'Shirts',
+      image: '/productImages/p5.jpg',
+      price: 1500,
+      countInStock: 10,
+      brand: 'Puma',
+      rating: '2.5',
+      numReviews: 20,
+      description: 'High Quality Slim Shirts'
+		},
+		{
+			_id: 6,
+      name: 'Nike Pants',
+      category: 'Pants',
+      image: '/productImages/p6.jpg',
+      price: 1000,
+      countInStock: 10,
+      brand: 'Nice',
+      rating: '3.5',
+      numReviews: 17,
+      description: 'Best pants quality'
+		},
+		{
+			_id: 7,
+      name: 'Versace Shirts',
+      category: 'Versace',
+      image: '/productImages/p7.jpg',
+      price: 1000,
+      countInStock: 15,
+      brand: 'Varsace',
+      rating: '4.5',
+      numReviews: 11,
+      description: 'High Quality Pants'
+		},
+		{
+			_id: 8,
+      name: 'Zara Clothing',
+      category: 'female t-shirts',
+      image: '/productImages/p8.jpg',
+      price: 100,
+      countInStock: 10,
+      brand: 'Zara',
+      rating: '4.5',
+      numReviews: 20,
+      description: 'High Quality  T-Shirts'
+		},
+		{
+			_id: 9,
+      name: 'Gucci Design',
+      category: 'Gucci',
+      image: '/productImages/p9.jpg',
+      price: 100,
+      countInStock: 10,
+      brand: 'Gucci',
+      rating: '4.5',
+      numReviews: 11,
+      description: 'High Quality Slim Shirts'
+		},
+		{
+			_id: 10,
+      name: 'Gucci Design',
+      category: 'Shirts',
+      image: '/productImages/p10.jpg',
+      price: 100,
+      countInStock: 10,
+      brand: 'Gucci',
+      rating: '4.5',
+      numReviews: 11,
+      description: 'High Quality Slim Shirts'
+		},
+		{
+			_id: 11,
+      name: 'Gucci T-shirt',
+      category: 'Shirts',
+      image: '/productImages/p11.jpg',
+      price: 100,
+      countInStock: 10,
+      brand: 'Gucci',
+      rating: '3.5',
+      numReviews: 11,
+      description: 'High Quality Slim Shirts'
+		},
+		{
+			_id: 12,
+      name: 'Versace T-shirt',
+      category: 'Versace',
+      image: '/productImages/p12.jpg',
+      price: 1000,
+      countInStock: 6,
+      brand: '',
+      rating: '1.5',
+      numReviews: 11,
+      description: 'Tob Best quality Long Sleeve Shirts'
+		},
+  ],
+};
+
+export default data;
